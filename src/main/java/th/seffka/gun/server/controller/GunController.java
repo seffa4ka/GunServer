0f6a37aeadf9ecffcbd415630077be051ed25250
seffka <th-seffka@mail.ru>
2016-09-25 12:47:55 +0600

    It works
    New: IDEA, Tomcat server.

--- src/main/java/th/seffka/gun/server/controller/GunController.java
+++ src/main/java/th/seffka/gun/server/controller/GunController.java
@@ -1,14 +1,17 @@
 package th.seffka.gun.server.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/gun")
 public class GunController {
     @RequestMapping(value = "test", method = RequestMethod.GET)
-    public String getTest() {
+    @ResponseBody
+    public String getTest(ModelMap mdel) {
         return "ok";
     }
 }
